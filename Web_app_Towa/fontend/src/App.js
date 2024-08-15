806bc5a67a73a2d4fa728dbd31ba9d7cd2b53c0a
o0Poseidon0o <90851953+o0Poseidon0o@users.noreply.github.com>
2024-08-15 16:37:31 +0700

    React
    web cong ty

---
+++ Web_app_Towa/fontend/src/App.js
@@ -0,0 +1,13 @@
+import Layoutmenu from "./compoments/Layout/Layoutmenu";
+import Headermain from "./compoments/Layout/Headermain";
+
+function App() {
+  return (
+    <div>
+      <Headermain></Headermain>
+      <Layoutmenu></Layoutmenu>
+    </div>
+  );
+}
+
+export default App;
